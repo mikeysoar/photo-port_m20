83e4493284a10456a6084c4ff375ed7e85fd9393
Mikeysoar <mwclimb@gmail.com>
2021-08-31 15:25:22 -0600

    ?

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -19,7 +19,7 @@ function Nav() {
         <header className="flex-row px-1">
             <h2>
                 <a data-testid="link" href="/">
-                    <span role="img" aria-label="camera"> 📸</span> Oh Snap!
+                    <span role="img" aria-label="camera">📸</span> Oh Snap!
                 </a>
             </h2>
             <nav>
