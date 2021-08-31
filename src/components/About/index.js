3cb17c0c821b9a7da319bdaa1ea1e59d66d915c3
Mikeysoar <mwclimb@gmail.com>
2021-08-31 14:51:57 -0600

    about section and nav section 20210831-1451

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React from 'react'
 import coverImage from "../../assets/cover/cover-image.jpg";
 
 function About() {
     return (
-        <section className="my-5">
-            <h1 id = "about">Who am I?</h1>
-            <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover"/>
+        <section className="my-5" >
+            <h1 id="about">Who am I?</h1>
+            <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
+            <div className="my-2">
+                <p>
+                    Lorem ipson
+                </p>
+            </div>
         </section>
-    );
+    )
 }
 
-export default About;
+export default About
